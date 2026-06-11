ae7c88224053063b7d7ca1e3eed850e331233dfd
sonms <71806591+sonms@users.noreply.github.com>
2026-06-11 11:17:05 +0900

    [Merge] 카메라 및 심박수 측정 기능을 위한 프로젝트 구성 및 종속성 업데이트 (#2)
    - `.gitignore` 파일에 Android, IntelliJ 및 OS별 파일에 대한 포괄적인 규칙을 추가했습니다.
    
    - `libs.versions.toml` 파일에 CameraX, Coroutines 및 Lifecycle Compose 종속성을 추가했습니다.
    
    - `heartrate` 및 `app` 모듈에 새로운 카메라 및 코루틴 종속성을 구성했습니다.
    
    - `app` 모듈의 종속성으로 `:heartrate` 모듈을 포함했습니다.
    
    - `AndroidManifest.xml` 파일에 필요한 카메라 권한 및 하드웨어 기능을 추가했습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,20 @@ android {
 
 dependencies {
 
+    implementation(project(":heartrate"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
